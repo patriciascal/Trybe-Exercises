f5649de5dd369c88e1349694d402f83bbadd1f90
Patricia <patriciascal@gmail.com>
2022-08-24 23:08:21 -0300

    Adiciona exercicio 9 e renomeia os outros

---
+++ Fundamentos/bloco04-introducao-javascript/dia01-primeiros-passos-JS/exercicio9.js
@@ -0,0 +1,11 @@
+// IDENTIFICANDO NUMERO PAR ENTRE 3 VALORES // 
+
+const numbA = 3;
+const numbB = 6;
+const numbC = 4;
+
+if (numbA % 2 !== 0 || numbB % 2 !== 0 || numbC % 2 !== 0) {
+    console.log ('True')
+} else {
+    console.log ('False')
+}
